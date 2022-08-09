8220ad9cb7152e544e547a5a413d55db30c9776e
Collin Reeser <collin.reeser@gmail.com>
2022-08-09 05:26:52 +0000

    Configure building against LLVM

--- src/berk.cpp
+++ src/berk.cpp
@@ -12,6 +12,18 @@
 #include <tao/pegtl/contrib/parse_tree_to_dot.hpp>
 #include <tao/pegtl/contrib/trace.hpp>
 
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
 #include "config.h"
 
 // fn main(): i32 {
