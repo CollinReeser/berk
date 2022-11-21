0b02034b6f7f5a436ab8976144083b7b47984926
Collin Reeser <collin.reeser@gmail.com>
2022-11-21 23:53:33 +0000

    Skeleton typechecking

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,7 @@ AST for the berk language.
 
 type berk_type =
   | Int
+  | Bool
 
 type ident_t = string
 
@@ -13,6 +14,7 @@ type cond_block = {
 }
 and expr =
   | ValInt of int
+  | ValBool of bool
   | Add of expr * expr
   | Sub of expr * expr
   | Mul of expr * expr
@@ -29,6 +31,7 @@ and stmt =
       else_if_blocks: cond_block list;
       else_block: (stmt list) option;
     }
+  | ResolveStmt of expr
 ;;
 
 type func_param = {
@@ -42,35 +45,95 @@ type func_ast = {
   f_stmts: stmt list;
 }
 
-let common_type_of lhs_type rhs_type =
-  match (lhs_type, rhs_type) with
+let rec common_type_of_lr lhs rhs =
+  match (lhs, rhs) with
   | (Some(Int), Some(Int)) -> Some(Int)
-  | (None, _) -> None
-  | (_, None) -> None
+  | (Some(Bool), Some(Bool)) -> Some(Bool)
+  | _ -> None
 
-let rec type_check_expr exp =
+and common_type_of_lst lst =
+  match lst with
+  | [] -> None
+  | [x] -> x
+  | x::xs -> List.fold_left common_type_of_lr x xs
+
+
+let rec type_check_if_expr_stmts stmts =
+  let resolve_stmt_types =
+    List.filter_map (
+      fun st ->
+        match st with
+        | ResolveStmt ex -> Some(type_check_expr ex)
+        | _ -> None
+    ) stmts
+  in
+  match resolve_stmt_types with
+  | [] -> None
+  | [x] -> x
+  | x::xs -> List.fold_left common_type_of_lr x xs
+and type_check_expr exp =
   match exp with
   | ValInt(_) -> Some(Int)
+  | ValBool(_) -> Some(Bool)
   | Add(lhs, rhs) ->
       let lhs_type = type_check_expr lhs in
       let rhs_type = type_check_expr rhs in
-      common_type_of lhs_type rhs_type
+      common_type_of_lr lhs_type rhs_type
   | Sub(lhs, rhs) ->
       let lhs_type = type_check_expr lhs in
       let rhs_type = type_check_expr rhs in
-      common_type_of lhs_type rhs_type
+      common_type_of_lr lhs_type rhs_type
   | Mul(lhs, rhs) ->
       let lhs_type = type_check_expr lhs in
       let rhs_type = type_check_expr rhs in
-      common_type_of lhs_type rhs_type
-
-  (* | IfExpr({
+      common_type_of_lr lhs_type rhs_type
+  | IfExpr({
       if_block = {cond = if_cond; stmts = if_stmts};
       else_if_blocks;
       else_block;
-    }) -> *)
-
-  | IfExpr(_) -> Some(Int)
+    }) ->
+      let if_cond_type = type_check_expr if_cond in
+      let if_stmts_type = type_check_if_expr_stmts if_stmts in
+      let else_if_cond_type = (
+        List.fold_left (
+          fun lhs rhs ->
+            match (lhs, rhs) with
+            | (Some(Bool), Some(Bool)) -> Some(Bool)
+            | _ -> None
+        )
+        (Some(Bool))
+        (
+          List.map (
+            fun {cond; stmts = _} ->
+              type_check_expr cond
+          ) else_if_blocks
+        )
+      ) in
+      let else_if_stmts_type =
+        let else_if_stmts_types =
+          List.map
+            type_check_if_expr_stmts (
+              List.map
+                (fun {cond = _; stmts} -> stmts)
+                else_if_blocks
+            )
+        in
+        match else_if_stmts_types with
+        | [] -> None
+        | [x] -> x
+        | x::xs -> List.fold_left common_type_of_lr x xs
+      in
+      let else_stmts_type =
+        match else_block with
+        | None -> None
+        | Some(stmts) -> type_check_if_expr_stmts stmts
+      in
+      match (if_cond_type, else_if_cond_type) with
+      | (Some(Bool), Some(Bool)) ->
+          common_type_of_lst [
+            if_stmts_type; else_if_stmts_type; else_stmts_type
+          ]
+      | _ -> None
 ;;
 
 
@@ -94,12 +157,12 @@ let build_example_ast =
       );
       ExprStmt(
         IfExpr({
-          if_block = {cond = ValInt(30); stmts = [ExprStmt(ValInt(31))]};
+          if_block = {cond = ValInt(30); stmts = [ResolveStmt(ValInt(31))]};
           else_if_blocks = [
-            {cond = ValInt(32); stmts = [ExprStmt(ValInt(33))]};
-            {cond = ValInt(34); stmts = [ExprStmt(ValInt(35))]};
+            {cond = ValInt(32); stmts = [ResolveStmt(ValInt(33))]};
+            {cond = ValInt(34); stmts = [ResolveStmt(ValInt(35))]};
           ];
-          else_block = Some([ExprStmt(ValInt(35))])
+          else_block = Some([ResolveStmt(ValInt(35))])
         })
       );
       DeclDef(
@@ -132,6 +195,7 @@ let build_example_ast =
 let print_berk_type berk_type =
   match berk_type with
   | Int -> Printf.printf "int"
+  | Bool -> Printf.printf "bool"
 ;;
 
 let print_func_param {p_name; p_type} =
@@ -155,6 +219,7 @@ let rec print_expr ind ex =
   | Sub (lhs, rhs) -> print_expr "" lhs; Printf.printf " - "; print_expr "" rhs
   | Mul (lhs, rhs) -> print_expr "" lhs; Printf.printf " * "; print_expr "" rhs
   | ValInt (value) -> Printf.printf "%d" value
+  | ValBool (value) -> Printf.printf "%B" value
   | IfExpr ({
       if_block = {
         cond = if_cond; stmts = if_stmts
@@ -195,6 +260,10 @@ and print_stmt ind stmt =
       Printf.printf "%s" ind;
       print_expr ind ex;
       Printf.printf ";\n";
+  | ResolveStmt (ex) ->
+      Printf.printf "%sresolve " ind;
+      print_expr ind ex;
+      Printf.printf ";\n";
   | IfStmt ({
       if_block = {
         cond = if_cond; stmts = if_stmts
@@ -233,13 +302,72 @@ let print_func_ast {f_name; f_params; f_stmts} =
 
 let main =
   print_func_ast build_example_ast;
-  let ex_to_check = Add(ValInt(5), ValInt(6)) in
-  Printf.printf "Expression [";
-  print_expr "" ex_to_check;
-  Printf.printf "] typechecks to: ";
-  match type_check_expr ex_to_check with
-  | None -> Printf.printf "<typecheck failed>"
-  | Some(t) -> print_berk_type t; Printf.printf "\n"
+
+  (
+    let ex_to_check = Add(ValInt(5), ValInt(6)) in
+    Printf.printf "Expression [";
+    print_expr "" ex_to_check;
+    Printf.printf "] typechecks to: ";
+    match type_check_expr ex_to_check with
+    | None -> Printf.printf "<typecheck failed>\n"
+    | Some(t) -> print_berk_type t; Printf.printf "\n";
+  );
+
+  (
+    let if_ex_to_check =
+      IfExpr({
+        if_block = {cond = ValBool(true); stmts = [ResolveStmt(ValInt(31))]};
+        else_if_blocks = [
+          {cond = ValBool(true); stmts = [ResolveStmt(ValInt(33))]};
+          {cond = ValBool(true); stmts = [ResolveStmt(ValInt(35))]};
+        ];
+        else_block = Some([ResolveStmt(ValInt(35))])
+      })
+    in
+    Printf.printf "Expression [";
+    print_expr "" if_ex_to_check;
+    Printf.printf "] typechecks to: ";
+    match type_check_expr if_ex_to_check with
+    | None -> Printf.printf "<typecheck failed>\n"
+    | Some(t) -> print_berk_type t; Printf.printf "\n";
+  );
+
+  (
+    let if_ex_to_check_2 =
+      IfExpr({
+        if_block = {cond = ValBool(true); stmts = [ResolveStmt(ValInt(31))]};
+        else_if_blocks = [
+          {cond = ValBool(true); stmts = [ResolveStmt(ValBool(true))]};
+          {cond = ValBool(true); stmts = [ResolveStmt(ValInt(35))]};
+        ];
+        else_block = Some([ResolveStmt(ValBool(false))])
+      })
+    in
+    Printf.printf "Expression [";
+    print_expr "" if_ex_to_check_2;
+    Printf.printf "] typechecks to: ";
+    match type_check_expr if_ex_to_check_2 with
+    | None -> Printf.printf "<typecheck failed>\n"
+    | Some(t) -> print_berk_type t; Printf.printf "\n";
+  );
+
+  (
+    let if_ex_to_check_3 =
+      IfExpr({
+        if_block = {cond = ValBool(true); stmts = [ResolveStmt(ValBool(true))]};
+        else_if_blocks = [
+          {cond = ValBool(true); stmts = [ResolveStmt(ValBool(false))]};
+        ];
+        else_block = Some([ResolveStmt(ValBool(false))])
+      })
+    in
+    Printf.printf "Expression [";
+    print_expr "" if_ex_to_check_3;
+    Printf.printf "] typechecks to: ";
+    match type_check_expr if_ex_to_check_3 with
+    | None -> Printf.printf "<typecheck failed>\n"
+    | Some(t) -> print_berk_type t; Printf.printf "\n";
+  );
 ;;
 
 main;;
