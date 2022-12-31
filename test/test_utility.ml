efd84237695df38796d112f41598b11da928db6f
Collin Reeser <collin.reeser@gmail.com>
2022-12-31 17:31:15 +0000

    Skeleton test infrastructure working

---
+++ test/test_utility.ml
@@ -0,0 +1,14 @@
+open Berk_ocaml.Utility
+
+let test_tuplify () =
+  Alcotest.(check (list (pair int int))) "tuplify"
+    [(1, 2); (1, 3); (1, 4); (2, 3); (2, 4); (3, 4)]
+    (List.sort compare (list_to_2_tuples [1; 2; 3; 4]))
+
+let () =
+  let open Alcotest in
+  run "Utility" [
+    "tuplify", [
+        test_case "tuplify" `Quick test_tuplify;
+      ];
+    ]
