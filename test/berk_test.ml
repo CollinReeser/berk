4f4e4f1d96d6b2c44df81f18de796b31b5a1657b
Collin Reeser <collin.reeser@gmail.com>
2022-12-31 17:04:36 +0000

    Testing infrastructure skeleton

---
+++ test/berk_test.ml
@@ -0,0 +1,12 @@
+
+
+let test_lowercase () =
+  Alcotest.(check string) "same string" "hello!" (String.lowercase_ascii "hELLO!")
+
+let () =
+  let open Alcotest in
+  run "Utils" [
+    "string-case", [
+        test_case "Lower case" `Quick test_lowercase;
+      ];
+    ]
