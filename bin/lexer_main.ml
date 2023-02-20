687de048fc5c33559344da4836cbc9ad3e2cc536
Collin Reeser <collin.reeser@gmail.com>
2023-02-20 10:12:31 +0000

    Defining minimum lexer definition for basic hello world program

--- bin/lexer_main.ml
+++ bin/lexer_main.ml
@@ -4,9 +4,21 @@ type position = {
 }
 
 and token =
-| Number of position * string
-| Ident of position * string
-| Op of position * string
+| KWExtern of position
+| LParen of position
+| RParen of position
+| LBrace of position
+| RBrace of position
+| Comma of position
+| ColonEqual of position
+| Colon of position
+| Semicolon of position
+| TriEllipses of position
+| BiEllipses of position
+| Integer of position * string
+| LowIdent of position * string
+| CapIdent of position * string
+| String of position * string
 
 
 let get_pos buf =
@@ -46,20 +58,56 @@ let rec print_tokens tokens =
   let fmt_pos (pos : position) =
     let (beg_line, beg_col, fin_line, fin_col) = lines_cols pos in
     Printf.sprintf
-      "[bL:%d, bC:%d; eL:%d, eC:%d]"
+      "[%d:%d ; %d:%d]"
       beg_line beg_col fin_line fin_col
   in
 
   begin match tokens with
   | [] -> ()
-  | Number(p, s) :: rest ->
-      Printf.printf "Number(%s) : %s\n" s (fmt_pos p) ;
+  | KWExtern(p) :: rest ->
+      Printf.printf "extern (kw) : %s\n" (fmt_pos p) ;
       print_tokens rest
-  | Ident(p, s) :: rest ->
-      Printf.printf "Indent(%s) : %s\n" s (fmt_pos p) ;
+  | LParen(p) :: rest ->
+      Printf.printf "( (syn) : %s\n" (fmt_pos p) ;
       print_tokens rest
-  | Op(p, s) :: rest ->
-      Printf.printf "Op(%s) : %s\n" s (fmt_pos p) ;
+  | RParen(p) :: rest ->
+      Printf.printf ") (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | LBrace(p) :: rest ->
+      Printf.printf "{ (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | RBrace(p) :: rest ->
+      Printf.printf "} (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | Comma(p) :: rest ->
+      Printf.printf ", (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | ColonEqual(p) :: rest ->
+      Printf.printf ":= (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | Colon(p) :: rest ->
+      Printf.printf ": (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | Semicolon(p) :: rest ->
+      Printf.printf "; (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | TriEllipses(p) :: rest ->
+      Printf.printf "... (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | BiEllipses(p) :: rest ->
+      Printf.printf ".. (syn) : %s\n" (fmt_pos p) ;
+      print_tokens rest
+  | Integer(p, s) :: rest ->
+      Printf.printf "%s (integer) : %s\n" s (fmt_pos p) ;
+      print_tokens rest
+  | LowIdent(p, s) :: rest ->
+      Printf.printf "%s (low-ident) : %s\n" s (fmt_pos p) ;
+      print_tokens rest
+  | CapIdent(p, s) :: rest ->
+      Printf.printf "%s (cap-ident) : %s\n" s (fmt_pos p) ;
+      print_tokens rest
+  | String(p, s) :: rest ->
+      Printf.printf "%s (string) : %s\n" s (fmt_pos p) ;
       print_tokens rest
   end
 ;;
@@ -68,30 +116,75 @@ let rec print_tokens tokens =
 let tokenize buf =
   let digit = [%sedlex.regexp? '0' .. '9'] in
   let number = [%sedlex.regexp? Plus digit] in
-  let letter = [%sedlex.regexp? 'a' .. 'z' | 'A' .. 'Z'] in
+  let str_simple_inner = [%sedlex.regexp? Star(Compl('"' | '\\'))] in
+  let str_escape_inner =
+    [%sedlex.regexp?
+      Star('\\', any, Star(str_simple_inner))
+    ] in
+  let str_reg =
+    [%sedlex.regexp?
+      '"', str_simple_inner, str_escape_inner, '"'
+    ]
+  in
 
   let rec _tokenize buf tokens =
     begin match%sedlex buf with
+    (* Keywords *)
+    | "extern" ->
+        let tok = KWExtern(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+
+    (* Syntax *)
+    | "(" ->
+        let tok = LParen(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | ")" ->
+        let tok = RParen(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | "{" ->
+        let tok = LBrace(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | "}" ->
+        let tok = RBrace(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | "," ->
+        let tok = Comma(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | ":=" ->
+        let tok = ColonEqual(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | ":" ->
+        let tok = Colon(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | ";" ->
+        let tok = Semicolon(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | "..." ->
+        let tok = TriEllipses(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+    | ".." ->
+        let tok = BiEllipses(get_pos buf) in
+        _tokenize buf (tok :: tokens)
+
     | number ->
-        let (start, fin) = Sedlexing.loc buf in
-        Printf.printf "LOC:Number(...)[%d, %d]\n" start fin ;
-
         let lexeme = Sedlexing.Latin1.lexeme buf in
-        _tokenize buf ((Number(get_pos buf, lexeme)) :: tokens)
+        let tok = Integer(get_pos buf, lexeme) in
+        _tokenize buf (tok :: tokens)
 
-    | letter, Star ('A' .. 'Z' | 'a' .. 'z' | digit) ->
-        let (start, fin) = Sedlexing.loc buf in
-        Printf.printf "LOC:Ident(...)[%d, %d]\n" start fin ;
-
+    | 'A' .. 'Z', Star(id_continue) ->
         let lexeme = Sedlexing.Latin1.lexeme buf in
-        _tokenize buf ((Ident(get_pos buf, lexeme)) :: tokens)
+        let tok = CapIdent(get_pos buf, lexeme) in
+        _tokenize buf (tok :: tokens)
 
-    | Plus (Chars "+*-/") ->
-        let (start, fin) = Sedlexing.loc buf in
-        Printf.printf "LOC:Op(...)[%d, %d]\n" start fin ;
-
+    | ('_' | 'a' .. 'z'), Star(id_continue) ->
         let lexeme = Sedlexing.Latin1.lexeme buf in
-        _tokenize buf ((Op(get_pos buf, lexeme)) :: tokens)
+        let tok = LowIdent(get_pos buf, lexeme) in
+        _tokenize buf (tok :: tokens)
+
+    | str_reg ->
+        let lexeme = Sedlexing.Latin1.lexeme buf in
+        let tok = String(get_pos buf, lexeme) in
+        _tokenize buf (tok :: tokens)
 
     | Plus xml_blank ->
         _tokenize buf tokens
@@ -100,18 +193,41 @@ let tokenize buf =
         tokens
 
     | 128 .. 255 ->
-        failwith "Non ASCII"
+        let pos = get_pos buf in
+        let (line, col, _, _) = lines_cols pos in
 
-    | _ -> failwith "Unexpected character"
+        Printf.printf "Non-ASCII: L:%d, C:%d\n" line col ;
+
+        tokens
+
+    | _ ->
+        let pos = get_pos buf in
+        let (line, col, _, _) = lines_cols pos in
+
+        Printf.printf "Unexpected character: L:%d, C:%d\n" line col ;
+
+        tokens
     end
   in
 
   let tokens = _tokenize buf [] in
   List.rev tokens
+;;
 
 
 let () =
-  let text = "foobar A123Bfoo  \n  ++123Xbar/foo" in
+  let text = {|
+    extern fn printf(fmt: string, ...): i32;
+
+    fn main(): i8 {
+      let my_str := "Hello, world!\n";
+      printf(my_str);
+
+      return 0;
+    }
+  |} in
   let lexbuf = Sedlexing.Latin1.from_gen (Gen.of_string text) in
   let tokens = tokenize lexbuf in
   print_tokens tokens
+;;
+
