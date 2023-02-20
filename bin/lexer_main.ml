4e25efb71795981baca23e6968d87a140ae686d2
Collin Reeser <collin.reeser@gmail.com>
2023-02-20 01:48:30 +0000

    Tweak the tokenizing toy to actually yield tokens

--- bin/lexer_main.ml
+++ bin/lexer_main.ml
@@ -1,23 +1,61 @@
-let digit = [%sedlex.regexp? '0' .. '9']
-let number = [%sedlex.regexp? Plus digit]
+type token =
+| Number of string
+| Ident of string
+| Op of string
 
-let rec token buf =
+
+let rec print_tokens tokens =
+  begin match tokens with
+  | [] -> ()
+  | Number(s) :: rest ->
+      Printf.printf "Number(%s)\n" s ;
+      print_tokens rest
+  | Ident(s) :: rest ->
+      Printf.printf "Indent(%s)\n" s ;
+      print_tokens rest
+  | Op(s) :: rest ->
+      Printf.printf "Op(%s)\n" s ;
+      print_tokens rest
+  end
+
+
+let tokenize buf =
+  let digit = [%sedlex.regexp? '0' .. '9'] in
+  let number = [%sedlex.regexp? Plus digit] in
   let letter = [%sedlex.regexp? 'a' .. 'z' | 'A' .. 'Z'] in
-  match%sedlex buf with
+
+  let rec _tokenize buf tokens =
+    begin match%sedlex buf with
     | number ->
-        Printf.printf "Number %s\n" (Sedlexing.Latin1.lexeme buf);
-        token buf
+        let lexeme = Sedlexing.Latin1.lexeme buf in
+        _tokenize buf ((Number(lexeme)) :: tokens)
+
     | letter, Star ('A' .. 'Z' | 'a' .. 'z' | digit) ->
-        Printf.printf "Ident %s\n" (Sedlexing.Latin1.lexeme buf);
-        token buf
-    | Plus xml_blank -> token buf
+        let lexeme = Sedlexing.Latin1.lexeme buf in
+        _tokenize buf ((Ident(lexeme)) :: tokens)
+
     | Plus (Chars "+*-/") ->
-        Printf.printf "Op %s\n" (Sedlexing.Latin1.lexeme buf);
-        token buf
-    | 128 .. 255 -> print_endline "Non ASCII"
-    | eof -> print_endline "EOF"
+        let lexeme = Sedlexing.Latin1.lexeme buf in
+        _tokenize buf ((Op(lexeme)) :: tokens)
+
+    | Plus xml_blank ->
+        _tokenize buf tokens
+
+    | eof ->
+        tokens
+
+    | 128 .. 255 ->
+        failwith "Non ASCII"
+
     | _ -> failwith "Unexpected character"
+    end
+  in
+
+  let tokens = _tokenize buf [] in
+  List.rev tokens
+
 
 let () =
   let lexbuf = Sedlexing.Latin1.from_string "foobar A123Bfoo  ++123Xbar/foo" in
-  token lexbuf
+  let tokens = tokenize lexbuf in
+  print_tokens tokens
